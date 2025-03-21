ae8b518e42976ed87455193eb49e2e187af013d7
oosoriooliver <luisalianis@hotmail.com>
2025-03-20 19:55:01 -0500

    se realizo arrays para hacer un .map

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,36 @@
+import { useState } from "react";
 import { Contador } from "./componentes/Contador";
 import { Listas } from "./componentes/Listas";
 import { Texto } from "./componentes/Texto";
 import { Usuario } from "./componentes/usuario";
 
 export const App = () => {
+  const listado = [
+    { nombre: "oscar osorio", edad: "46", nacionalidad: "colombiano" },
+    { nombre: "Luisa Osorio", edad: "19", nacionalidad: "colombiana" },
+    { nombre: "Gladys Oliver", edad: "60", nacionalidad: "argentina" },
+    { nombre: "Lianis fernanda", edad: "14", nacionalidad: "españa" },
+    { nombre: "eliana patricia", edad: "54", nacionalidad: "EE.UU" },
+  ];
+
+  const [usuario, setusuario] = useState(listado);
+
   return (
     <>
-      {/* <Usuario nombre="oscar osorio" edad="46" nacionalidad="colombiano" />
-      <Usuario nombre="Luisa osorio" edad="19" nacionalidad="colombiana" />
+      {usuario.map((user) => (
+        <Usuario
+          key={user.nombre}
+          nombre={user.nombre}
+          edad={user.edad}
+          nacionalidad={user.nacionalidad}
+        ></Usuario>
+      ))}
+
       <Contador />
       <hr />
-      <Texto/>
-      <hr /> */}
-      <Listas/>
+      <Texto />
+      <hr />
+      <Listas />
     </>
   );
 };
