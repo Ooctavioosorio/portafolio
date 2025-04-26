daf3559d6b428322481cd7de32afdcde63290442
oscar osorio <hansell.ramos@gmail.com>
2025-04-26 16:40:36 -0500

    se retita service worker del proyecto

--- src/main.jsx
+++ src/main.jsx
@@ -4,18 +4,6 @@ import './index.css'
 import './App.css'
 import App from './App.jsx'
 
-// Desactivación permanente del Service Worker en todos los entornos
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.getRegistrations().then(function(registrations) {
-    for(let registration of registrations) {
-      registration.unregister();
-      console.log('Service Worker desactivado permanentemente');
-    }
-  }).catch(function(err) {
-    console.log('Error al desactivar Service Worker:', err);
-  });
-}
-
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
